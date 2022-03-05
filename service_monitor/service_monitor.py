94e15dc1e0e7cdb386618e76827ce18d81b61c45
Karl <kc0bfv@gmail.com>
2022-03-05 16:19:50 -0600

    Add unitttest and improve some handling

--- service_monitor/service_monitor.py
+++ service_monitor/service_monitor.py
@@ -3,28 +3,63 @@
 import os
 import urllib.request
 import urllib.error
+import unittest
 
 import boto3
 
-def alert_failed(msg_txt="Connection fail"):
-    region = os.environ.get("region")
-    profile = os.environ.get("profile")
-    from_addr = os.environ["email_from"]
-    to_addr = os.environ["email_to"]
-    subject = "ALERT: Service Monitor"
+SETTINGS = None
+def get_settings():
+    global SETTINGS
+    if SETTINGS is None:
+        SETTINGS = dict()
+        SETTINGS["region"] = os.environ.get("region")
+        SETTINGS["profile"] = os.environ.get("profile")
+        SETTINGS["from_addr"] = os.environ["email_from"]
+        SETTINGS["to_addr"] = os.environ["email_to"]
+        SETTINGS["url"] = os.environ["monitor_url"]
+        SETTINGS["expect"] = int(os.environ["expect_code"])
+    return SETTINGS
+    
 
-    sess = boto3.session.Session(region_name=region, profile_name=profile)
+def alert_failed(msg_txt: str = "Connection fail") -> None:
+    settings = get_settings()
+    subject = "ALERT: Service Monitor"
+    body = f"URL: {settings['url']}\nMSG: {msg_txt}"
+
+    sess = boto3.session.Session(region_name=settings["region"],
+            profile_name=settings["profile"])
     ses = sess.client("ses")
     response = ses.send_email(
-        Source = from_addr,
-        Destination = {"ToAddresses": [to_addr]},
+        Source = settings["from_addr"],
+        Destination = {"ToAddresses": [settings["to_addr"]]},
         Message = {
             "Subject": {"Data": subject},
-            "Body": {"Text": {"Data": msg_txt} },
+            "Body": {"Text": {"Data": body} },
         }
     )
 
     print(f"SES Response: {response}")
+
+def run_monitor():
+    settings = get_settings()
+    req = urllib.request.Request(settings["url"])
+    try:
+        with urllib.request.urlopen(req, timeout=1) as urlf:
+            data = urlf.read()
+    except urllib.error.HTTPError as err:
+        if err.status == settings["expect"]:
+            print(f"Success - {settings['expect']} as expected")
+            return True
+        else:
+            raise err
+    else:
+        print("Data received: {}".format(len(data)))
+        if settings["expect"] == 200:
+            return True
+        else:
+            raise RuntimeError("Status was 200, unexpectedly")
+    print("End of run monitor")
+    return False
 
 def service_monitor(event, context):
     try:
@@ -36,26 +71,124 @@ def service_monitor(event, context):
     except:
         alert_failed("Connection fail: non-BaseException?")
         raise
-        
-
-def run_monitor():
-    url = os.environ["monitor_url"]
-    expect = int(os.environ["expect_code"])
-    req = urllib.request.Request(url)
-    try:
-        with urllib.request.urlopen(req, timeout=1) as urlf:
-            data = urlf.read()
-    except urllib.error.HTTPError as err:
-        if err.status == expect:
-            print(f"Success - {expect} as expected")
-            return True
-        else:
-            raise err
-    else:
-        print("Data received: {}".format(len(data)))
-        return True
-    print("End of run monitor")
-    return False
 
 if __name__ == "__main__":
     service_monitor((),())
+
+
+
+
+def makeFakeBoto():
+    class FakeBoto3:
+        class session:
+            class Session:
+                all_sessions = list()
+                def __init__(self, region_name, profile_name):
+                    FakeBoto3.session.Session.all_sessions.append(self)
+                    self.region_name = region_name
+                    self.profile_name = profile_name
+                def client(self, service):
+                    self.service = service
+                    return self
+                def send_email(self, Source, Destination, Message):
+                    self.send_email = {"src": Source, "dest": Destination,
+                        "msg": Message}
+                    return "Fake email sent"
+    return FakeBoto3
+
+class TestClass(unittest.TestCase):
+    def test_with_200_expect_200(self):
+        global boto3, SETTINGS
+
+        fake_set = {
+            "region": "fake_reg",
+            "profile": "fake_prof",
+            "from_addr": "from@from.com",
+            "to_addr": "to@to.com",
+            "url": "https://google.com/",
+            "expect": 200,
+        }
+
+        boto3 = makeFakeBoto()
+        SETTINGS = fake_set
+
+        service_monitor((),())
+
+        self.assertEqual(len(boto3.session.Session.all_sessions), 0)
+
+    def test_with_200_expect_400(self):
+        global boto3, SETTINGS
+
+        fake_set = {
+            "region": "fake_reg",
+            "profile": "fake_prof",
+            "from_addr": "from@from.com",
+            "to_addr": "to@to.com",
+            "url": "https://google.com/",
+            "expect": 400,
+        }
+
+        boto3 = makeFakeBoto()
+        SETTINGS = fake_set
+
+        with self.assertRaises(RuntimeError):
+            service_monitor((),())
+
+        self.assertEqual(len(boto3.session.Session.all_sessions), 1)
+        
+        out_sess = boto3.session.Session.all_sessions[0]
+
+        self.assertEqual(out_sess.region_name, fake_set["region"])
+        self.assertEqual(out_sess.profile_name, fake_set["profile"])
+        self.assertEqual(out_sess.service, "ses")
+        self.assertEqual(out_sess.send_email["src"], fake_set["from_addr"])
+        self.assertEqual(out_sess.send_email["dest"]["ToAddresses"], [fake_set["to_addr"]])
+        print(out_sess.send_email["msg"])
+
+    def test_with_404_expect_404(self):
+        global boto3, SETTINGS
+
+        fake_set = {
+            "region": "fake_reg",
+            "profile": "fake_prof",
+            "from_addr": "from@from.com",
+            "to_addr": "to@to.com",
+            "url": "https://google.com/WEKNenWENWEweklj",
+            "expect": 404,
+        }
+
+        boto3 = makeFakeBoto()
+        SETTINGS = fake_set
+
+        service_monitor((),())
+
+        self.assertEqual(len(boto3.session.Session.all_sessions), 0)
+
+    def test_with_404_expect_300(self):
+        global boto3, SETTINGS
+
+        fake_set = {
+            "region": "fake_reg",
+            "profile": "fake_prof",
+            "from_addr": "from@from.com",
+            "to_addr": "to@to.com",
+            "url": "https://google.com/WEKNenWENWEweklj",
+            "expect": 300,
+        }
+
+        boto3 = makeFakeBoto()
+        SETTINGS = fake_set
+
+        with self.assertRaises(urllib.error.HTTPError):
+            service_monitor((),())
+
+        self.assertEqual(len(boto3.session.Session.all_sessions), 1)
+        
+        out_sess = boto3.session.Session.all_sessions[0]
+
+        self.assertEqual(out_sess.region_name, fake_set["region"])
+        self.assertEqual(out_sess.profile_name, fake_set["profile"])
+        self.assertEqual(out_sess.service, "ses")
+        self.assertEqual(out_sess.send_email["src"], fake_set["from_addr"])
+        self.assertEqual(out_sess.send_email["dest"]["ToAddresses"], [fake_set["to_addr"]])
+        print(out_sess.send_email["msg"])
